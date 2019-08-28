432e71e38cd3a1d3b5ef60969a002440f8856855
Chandler Turner <chandlerturner@Chandlers-MacBook-Air.local>
2019-08-28 13:06:25 -0500

    Finished challenges for Chapter 🤘

--- overly-excited.js
+++ overly-excited.js
@@ -1,35 +1,41 @@
 console.log("test")
 
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray, punctuation) {
-    // Each time the for loop executes, you're going to add one more word to this string
+function addExcitement(theWordArray, punctuation, number) {
+
+    // Each time the for loop executes, you're going to add one more word to this string. Uses 1 to offset the array vs 0 would give you one more than stated. 
     let buildMeUp = ""
+    let symbol = punctuation
+    for (let i = 1; i < number; i++) {
+        symbol = punctuation + symbol
+    }
+
+
 
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
-        // buildMeUp = buildMeUp + " " + theWordArray[i]
-        if ((i+1) % 3 === 0){
-        buildMeUp = buildMeUp + " " + theWordArray[i] + punctuation
-    }
-    else {
-        buildMeUp = buildMeUp + " " + theWordArray[i]
-    }
+
+
+        if ((i + 1) % 3 === 0) {
+            buildMeUp = buildMeUp + " " + theWordArray[i] + symbol
+        }
+        else {
+            buildMeUp = buildMeUp + " " + theWordArray[i]
+        }
 
         // Print buildMeUp to the console
-    console.log(buildMeUp)
+        console.log(buildMeUp)
     }
 
 }
 
 // Invoke the function and pass in the array
 // I want to use a question mark
-addExcitement(sentence, "?", 4)
-
-//--------------------------Challenge------------------------------
+addExcitement(sentence, "🌈", 3)
